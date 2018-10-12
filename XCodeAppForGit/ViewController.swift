a1b7d723b7383025061828ab38c93a717a47612b
Tony Hutson <tony.hutson@tonyhutson.com>
2018-10-11 20:43:39 -0400

    Initial Commit

---
+++ XCodeAppForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeAppForGit
+//
+//  Created by Dwayne Hutson on 10/11/18.
+//  Copyright © 2018 Tony Hutson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
